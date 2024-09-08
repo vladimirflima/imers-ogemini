53c9d7ae7222bdf69250528b186b5785374b5130
Vladimir Lima <vladimirflima@outlook.com>
2024-09-08 11:19:01 -0300

    Segundo commit

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log("Rebeca Andrade")
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if (campoPesquisa == ""){
+        section.innerHTML = "<p>Nada foi encontrado</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+    
+    let resultados = "";
+    let marca = "";
+    let modelo = "";
+    let tags = "";
+
+    for (let dado of dados) {
+        marca = dado.marca.toLowerCase()
+        modelo = dado.modelo.toLowerCase()
+        tags = dado.tags.toLowerCase()
+
+        if (marca.includes(campoPesquisa) || modelo.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.modelo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.marca}</p>
+                <a href= target="_blank">${dado.caracteristica}</a>
+            </div>
+        `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+        
+
+    section.innerHTML = resultados;
+}
+
---
+++ dados.js
@@ -0,0 +1,42 @@
+let dados = [
+    {
+        marca: "Pinarello",
+        modelo: "Dogma F",
+        caracteristica: "Quadro em carbono T1100 1K, geometria agressiva, aerodinâmica otimizada.",
+        tags: "carbono aerodinâmica performance"
+      },
+      {
+        marca: "Specialized",
+        modelo: "S-Works Tarmac SL7",
+        caracteristica: "Quadro leve e rígido, aerodinâmica avançada, integrado com componentes eletrônicos.",
+        tags: "leve rigido eletrônico"
+      },
+      {
+        marca: "Canyon",
+        modelo: "Aeroad CFR Disc",
+        caracteristica: "Quadro aerodinâmico, freios a disco integrados, geometria race-ready.",
+        tags: "aerodinâmico freios disco"
+      },
+      {
+        marca: "Trek",
+        modelo: "Madone SLR",
+        caracteristica: "IsoSpeed para maior conforto, aerodinâmica avançada, quadro em carbono OCLV.",
+        tags: "conforto aerodinâmica carbono"
+      },
+      {
+        marca: "Cervélo",
+        modelo: "S5",
+        caracteristica: "Quadro aerodinâmico, integrado com componentes eletrônicos, geometria ajustável.",
+        tags: "aerodinâmico eletrônico ajustável"
+      },
+      {
+        marca: "Colnago",
+        modelo: "V3RS",
+        caracteristica: "Quadro em carbono, geometria race-ready, design italiano icônico.",
+        tags: "carbono design italiano"
+      }
+];
+
+
+    
+ 
